a4600f2d14a23a596aba7ac6d94a2455eb4889ad
Георгий Александров <g.aleksandrov@kpda.ru>
2026-02-12 11:08:47 +0300

    add keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,5 +5,8 @@ require("core.options")
 -- Загружаем настройки буфера обмена
 require("core.clipboard")
 
+-- Загружаем горячие клавиши
+require("core.keymaps")
+
 -- Загружаем плагины
 require("plugins")
---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,30 @@
+-- Горячие клавиши (keymaps)
+local keymap = vim.keymap.set
+
+-- Leader клавиша (по умолчанию \)
+vim.g.mapleader = " " -- Устанавливаем пробел как leader клавишу
+
+-- Навигация по вкладкам (буферам)
+-- Tab - следующая вкладка
+keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", {
+	desc = "Next buffer",
+	silent = true,
+})
+
+-- Shift+Tab - предыдущая вкладка
+keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", {
+	desc = "Previous buffer",
+	silent = true,
+})
+
+-- <leader>x - закрыть текущую вкладку
+keymap("n", "<leader>x", ":bdelete<CR>", {
+	desc = "Close buffer",
+	silent = true,
+})
+
+-- <leader>X - закрыть все вкладки кроме текущей
+keymap("n", "<leader>X", ":BufferLineCloseOthers<CR>", {
+	desc = "Close other buffers",
+	silent = true,
+})
