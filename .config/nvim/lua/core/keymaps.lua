4e73df64c51ec3039ff9d64b4467dfd1b0515147
Георгий Александров <g.aleksandrov@kpda.ru>
2026-04-13 18:42:52 +0300

    git restore текущего файла

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -32,3 +32,17 @@ keymap("n", "<leader>X", ":BufferLineCloseOthers<CR>", {
 -- Сохранять выделение после сдвига отступа (< и >)
 keymap("v", "<", "<gv", { desc = "Indent left and keep selection" })
 keymap("v", ">", ">gv", { desc = "Indent right and keep selection" })
+
+-- Git restore текущего файла
+keymap("n", "<leader>gr", function()
+	local file = vim.fn.expand("%:p")
+	local cmd = "git restore " .. vim.fn.shellescape(file)
+	local result = vim.fn.system(cmd)
+
+	if vim.v.shell_error == 0 then
+		vim.cmd("edit!") -- Перечитать файл
+		print("✓ Git restore: " .. vim.fn.expand("%:t"))
+	else
+		print("✗ Git restore failed: " .. result)
+	end
+end, { desc = "Git restore current file" })
