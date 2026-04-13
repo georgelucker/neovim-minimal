36e4d05c107ca47c766f81e59757617a065e6d1b
Георгий Александров <g.aleksandrov@kpda.ru>
2026-04-13 18:36:33 +0300

    Reapply "sops try2 6
    This reverts commit d00af0ae3b724af7fec43f969afd98d2f8ac9ff1.

--- .config/nvim/lua/plugins/sops.lua
+++ .config/nvim/lua/plugins/sops.lua
@@ -1,10 +1,19 @@
--- Автоматическая работа с SOPS-зашифрованными файлами
+-- Автоматическая работа с SOPS-зашифрованными файлами (nvim-sops)
 
 return {
-	"Glench/vim-sops",
-	lazy = false,
-	init = function()
-		-- Указываем путь к age ключу
+	"prismatic-koi/nvim-sops",
+	event = { "BufEnter" },
+	keys = {
+		{ "<leader>ef", vim.cmd.SopsEncrypt, desc = "Encrypt File" },
+		{ "<leader>df", vim.cmd.SopsDecrypt, desc = "Decrypt File" },
+	},
+	config = function()
 		vim.env.SOPS_AGE_KEY_FILE = vim.fn.expand("~/.config/sops/age/keys.txt")
+
+		require("nvim_sops").setup({
+			enabled = true,
+			debug = false,
+			binPath = "sops",
+		})
 	end,
 }
