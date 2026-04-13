59a14e66f7eb45c90bc709f6959fdb2cacf15d81
Георгий Александров <g.aleksandrov@kpda.ru>
2026-04-13 18:06:20 +0300

    Revert "sops try2 3
    This reverts commit 9abe59be0659be1df5e38cedde47f9b57bf4f122.

--- .config/nvim/lua/plugins/sops.lua
+++ .config/nvim/lua/plugins/sops.lua
@@ -1,6 +1,26 @@
--- Автоматическая работа с SOPS-зашифрованными файлами
+-- Автоматическая работа с SOPS-зашифрованными файлами (nvim-sops)
 
 return {
-	"trixnz/sops.nvim",
-	lazy = false, -- Загружать сразу при старте
+	"prismatic-koi/nvim-sops",
+	event = { "BufEnter" },
+	keys = {
+		-- Горячие клавиши для шифрования/расшифровки
+		{ "<leader>ef", vim.cmd.SopsEncrypt, desc = "Encrypt File" },
+		{ "<leader>df", vim.cmd.SopsDecrypt, desc = "Decrypt File" },
+	},
+	opts = {
+		-- Включить плагин
+		enabled = true,
+
+		-- Отладка (показывать больше информации)
+		debug = false,
+
+		-- Путь к sops (по умолчанию ищет в $PATH)
+		binPath = "sops",
+
+		-- Переменные окружения (если нужно переопределить)
+		defaults = {
+			ageKeyFile = "SOPS_AGE_KEY_FILE", -- ~/.config/sops/age/keys.txt
+		},
+	},
 }
