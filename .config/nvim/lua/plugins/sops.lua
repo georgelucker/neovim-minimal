a88345b430a32e5128ff00001bdce54b9e7488da
Георгий Александров <g.aleksandrov@kpda.ru>
2026-04-13 18:36:35 +0300

    Reapply "sops try2 5
    This reverts commit 0cc96d5eb2fdbdab5474563ccbd5e703eebbe936.

--- .config/nvim/lua/plugins/sops.lua
+++ .config/nvim/lua/plugins/sops.lua
@@ -4,15 +4,17 @@ return {
 	"prismatic-koi/nvim-sops",
 	event = { "BufEnter" },
 	keys = {
+		-- Горячие клавиши для шифрования/расшифровки
 		{ "<leader>ef", vim.cmd.SopsEncrypt, desc = "Encrypt File" },
 		{ "<leader>df", vim.cmd.SopsDecrypt, desc = "Decrypt File" },
 	},
 	config = function()
+		-- Устанавливаем переменную окружения перед запуском плагина
 		vim.env.SOPS_AGE_KEY_FILE = vim.fn.expand("~/.config/sops/age/keys.txt")
 
 		require("nvim_sops").setup({
 			enabled = true,
-			debug = false,
+			debug = true, -- Включаем отладку
 			binPath = "sops",
 		})
 	end,
