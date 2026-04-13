2226e51b0abbfca909c5cf8ea6911ae0fa93994e
Георгий Александров <g.aleksandrov@kpda.ru>
2026-04-13 18:39:51 +0300

    manual decr

---
+++ .config/nvim/lua/plugins/sops.lua
@@ -0,0 +1,20 @@
+-- Ручное шифрование/расшифрование SOPS файлов
+
+return {
+	"prismatic-koi/nvim-sops",
+	event = { "BufEnter" },
+	keys = {
+		{ "<leader>ef", vim.cmd.SopsEncrypt, desc = "Encrypt File" },
+		{ "<leader>df", vim.cmd.SopsDecrypt, desc = "Decrypt File" },
+	},
+	config = function()
+		-- Устанавливаем путь к age ключу
+		vim.env.SOPS_AGE_KEY_FILE = vim.fn.expand("~/.config/sops/age/keys.txt")
+
+		require("nvim_sops").setup({
+			enabled = true,
+			debug = false,
+			binPath = "sops",
+		})
+	end,
+}
