62dfad0782a13ef408756281603655f753b70672
Георгий Александров <g.aleksandrov@kpda.ru>
2026-08-26 17:44:45 +0300

    add 2

--- omzsh.sh
+++ omzsh.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sleep 2
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="clean"/' ~/.zshrc
